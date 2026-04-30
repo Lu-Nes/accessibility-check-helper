ec07a904374ed77b2a18b5b79d03a05db81569c7
Lu-Nes <lu.nestmann@gmail.com>
2026-04-30 21:57:51 +0200

    Klassen für Grundlayot ergänzt

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <main>
+    <main className="app">
       <h1>Accessibility Check Helper</h1>
 
-      <section>
+      <section className="input-section">
         <h2>HTML-Code prüfen</h2>
 
         <label htmlFor="html-input">HTML-Code eingeben</label>
@@ -15,7 +15,7 @@ function App() {
         <button>HTML prüfen</button>
       </section>
 
-      <section>
+      <section className="result-section">
         <h2>Ergebnisse</h2>
 
         <p>Noch keine Prüfung gestartet.</p>
